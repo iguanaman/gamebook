e985cb825dbcbad877fabd1db21654a9b99bbb17
Ry <iguanaman@users.noreply.github.com>
2026-05-07 09:14:51 +0100

    tweak(chrome): align undo arrowhead with loop terminus

--- engine.js
+++ engine.js
@@ -1269,7 +1269,7 @@ function renderShell(meta) {
         <div class="choices-footer" id="choices-footer"></div>
       </div>
     </div>
-    <button class="ui-icon-btn btn-undo-fixed undo-disabled" id="btn-undo"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><polyline points="3 7 3 13 9 13"/><path d="M3.51 13a9 9 0 1 0 2.13-9.36"/></svg></button>
+    <button class="ui-icon-btn btn-undo-fixed undo-disabled" id="btn-undo"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><polyline points="1 4 1 10 7 10"/><path d="M3.51 15a9 9 0 1 0 2.13-9.36L1 10"/></svg></button>
   `;
   const narrative = storyApp.querySelector('#narrative');
   if (narrative) {
